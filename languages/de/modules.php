eb837597e10fb14cb9bdef0db8fc3da729a1bcb7
Yanick Witschi <yanick.witschi@terminal42.ch>
2013-09-11 10:17:29 +0200

    Added german translation

---
+++ languages/de/modules.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  terminal42 gmbh 2013
+ * @author     terminal42 <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/**
+ * Extension folder
+ */
+$GLOBALS['TL_LANG']['MOD']['articletabs'] = array('Artikel in Tabs');
+
+
+/**
+ * Frontend modules
+ */
+$GLOBALS['TL_LANG']['FMD']['articletabs'] = array('Artikel in Tabs');
+
